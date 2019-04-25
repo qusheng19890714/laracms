3f9463a8a48cb62a3ddd4f7dd7042b326e855641
vagrant <vagrant@homestead>
2019-04-25 10:26:11 +0000

    upload config

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,6 +64,16 @@ return [
             'url' => env('AWS_URL'),
         ],
 
+
+        'admin' => [
+
+            'driver' => 'local',
+            'root'   => public_path('upload'),
+            'visibility' => 'public',
+            'url' => env('APP_URL').'/upload/',
+
+        ]
+
     ],
 
 ];
