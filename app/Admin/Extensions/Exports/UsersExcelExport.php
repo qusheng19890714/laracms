5c3c4f06fc9dabf2b6a72ff256e855bae8f89f52
vagrant <vagrant@homestead>
2019-05-14 08:51:09 +0000

    export

--- app/Admin/Extensions/Exports/UsersExcelExport.php
+++ app/Admin/Extensions/Exports/UsersExcelExport.php
@@ -2,37 +2,21 @@
 
 namespace App\Admin\Extensions\Export;
 
-use Maatwebsite\Excel\Excel;
-use Encore\Admin\Grid\Exporters\AbstractExporter;
+use Encore\Admin\Grid\Exporters\ExcelExporter;
 
-class UserExcelExport extends AbstractExporter
+class UserExcelExport extends ExcelExporter
 {
 
-    public function export()
-    {
-        $gridData = $this->getData();
+    protected $fileName = '用户列表.xlsx';
 
-        $data = [];
+    protected $columns = [
+        'id'      => 'ID',
+        'name'    => '用户名',
+        'email'   => 'Email',
+        'phone'   => '手机号码',
+        'created_at' => '注册时间'
+    ];
 
-        //导出表头
-        $data[] = ['ID', trans('user::user.name.label'), trans('user::user.email.label'), trans('user::user.phone.label'), trans('user::user.created_at.label')];
-
-        foreach ($gridData as $v)
-        {
-            $data[] = [$v['id'], $v['name'], $v['email'], $v['tel'], $v['created_at']];
-        }
-
-        // 导出excel
-        \Excel::create('用户数据', function($excel) use ($data) {
-
-            $excel->sheet('用户', function($sheet) use($data) {
-
-                $sheet->rows($data);
-
-            });
-
-        })->download('xls');
-    }
 
 }
 
